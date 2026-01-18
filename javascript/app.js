39e3e0246c1d0bc4382af28b3573badda731400f
Mohammad Saad Ali <mohammadsaadali786.92@gmail.com>
2026-01-18 15:56:21 +0500

    18 jan

---
+++ javascript/app.js
@@ -0,0 +1,21 @@
+/*
+document.writeln("saad")
+alert("rehman")
+alert("dakait")
+alert("uzair")
+alert("balouch") 
+*/
+
+
+// document.writeln("saad" + " " + "ali")
+// document.writeln("Fahad " + "Abdullah")
+
+// document.writeln("<h1> Saad Ali </h1>")
+// document.writeln("<i>" +  "abcd" +  "</i>")
+
+
+// document.writeln((2 + 2) * 4 + ("satti" + "VIP") - (3 * 4))
+// 16 satti VIP - 12
+
+// document.writeln(10 * "2satti")
+
